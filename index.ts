133f344963451ac744699fcfc3231641485cf575
PetterSa <petter.saelen@gmail.com>
2020-05-22 10:03:25 +0200

    feat: Help identify objects with property observer functionality
    By creating the WithPropertyObserver interface you can now identify types that have the observableForProperty function by using the mixin operator. Like this: `function(example: SomeClass & WithPropertyObserver){//do stuff}

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import {Observable, Subscriber} from 'rxjs';
 
+export interface WithPropertyObserver {
+    observableForProperty(property: keyof this): Observable<this[keyof this]>;
+}
+
 export default function ObservablePropertyChanges<AnyObject extends new (...args: any[]) => object>(anyObject: AnyObject) {
-    return class ClassWithPropertyObserver extends anyObject {
+    return class ClassWithPropertyObserver extends anyObject implements WithPropertyObserver {
         private propertySubscribers = new Map<keyof this, Subscriber<this[keyof this]>[]>();
 
         constructor(...props: any[]) {
